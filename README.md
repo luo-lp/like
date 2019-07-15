0348c1cd3b41215ac75c9f82437488f3f8808936
luo-lp <875223115@qq.com>
2019-07-15 14:37:30 +0800

    新增了一个获取innerText值的功能

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # like
 git clone https://github.com/luo-lp/like 下载
+设置用户名
+git config user.name "luo-lp"
+设置账号
+git config user.email "875223115@qq.com"
+
+git add . 
+
+git commit -m "新增的内容描述"
+
+git push
--- js/myjq.js
+++ js/myjq.js
@@ -1,3 +1,15 @@
+/*方法说明
+ *@auhor 作者
+ *@example 示例
+ *@link 链接
+ *@namespace 命名空间
+ *@requires 依赖模块
+ *@version 版本号
+ *@method 方法名
+ *@for 所属类名
+ *@param{参数类型}参数名 参数说明
+ *@return {返回值类型} 返回值说明
+*/
 // 在这里形成一个局部作用域，防止别人的代码影响我们自己的代码
 ;
 (function () {
@@ -16,13 +28,25 @@
         }
         this.length = dom.length;
     }
+    /**
+     * 一个for循环的封装
+     * @method each
+     * @param{fn}
+     * @return this
+     */
     Init.prototype.each = function (callback) {
         for (let i = 0; i < this.length; i++) {
             callback(i, this[i]);
         };
     }
+    /**
+     * 设置CSS样式以及获取样式 
+     * 只传入css属性名时可以返回该属性的值
+     * 传入css属性及值时会根据传入的样式及值设置css样式
+     * @method css
+     * 
+     */
     Init.prototype.css = function (key, val) {
-        // console.log(val.string)
         if (val === undefined) {
             return window.getComputedStyle(this[0])[key];
         } else {
@@ -50,30 +74,54 @@
         }
 
     }
+    /**
+     * 添加CSS样式名
+     * @method addClass
+     * @param {string}传入一个符合css类名命名规则的字符串
+     */
     Init.prototype.addClass = function (className) {
         this.each(function (i, e) {
             e.classList.add(className);
         });
         return this;
     }
+    /**
+     * 移除CSS样式名
+     * @method removeClass
+     * @param {string}传入一个符合css类名命名规则的字符串
+     */
     Init.prototype.removeClass = function (className) {
         this.each(function (i, e) {
             e.classList.remove(className);
         });
         return this;
     }
+    /**
+     * 切换CSS样式名
+     * 根据传入的类名判断该对象上是否存在改类名，如果有则移除，反之添加
+     * @method toggleClass
+     * @param {string}传入一个符合css类名命名规则的字符串
+     */
     Init.prototype.toggleClass = function (className) {
         this.each(function (i, e) {
             e.classList.toggle(className);
         });
         return this;
     }
+    /**
+     * 使自己显示
+     * @method show
+     */
     Init.prototype.show = function () {
         this.each(function (i, e) {
             e.style.display = 'block';
         });
         return this;
     }
+    /**
+     * 使自己隐藏
+     * @method hide
+     */
     Init.prototype.hide = function () {
         this.each(function (i, e) {
             e.style.display = 'none';
@@ -81,6 +129,10 @@
         });
         return this;
     }
+    /**
+     * 获取及设置当前对象的html内容
+     * @method html
+     */
     Init.prototype.html = function (element) {
         let arr = []
         this.each(function (i, e) {
@@ -91,10 +143,31 @@
                 e.innerHTML = element;
             }
         });
-        if(element === undefined){
+        if (element === undefined) {
             return arr;
-        }else{
+        } else {
             return this;
         }
     }
+    /**
+     * 获取及设置当前对象的text内容
+     * @method text
+     */
+    Init.prototype.text = function (element) {
+        let arr = []
+        this.each(function (i, e) {
+            if (element === undefined) {
+                console.log(e.innerText)
+                arr[i] = e.innerText
+            } else {
+                e.innerText = element;
+            }
+        });
+        if (element === undefined) {
+            return arr;
+        } else {
+            return this;
+        }
+    }
+
 })();
