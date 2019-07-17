0aeede489fb0ffa3c04b8310a8cf76c93b681c12
llp <875223115@qq.com>
2019-07-17 20:08:05 +0800

    新增一个用递归求斐波那契数列的方法

--- js/递归.js
+++ js/递归.js
@@ -6,3 +6,10 @@ function getn(n) {
     return n * getn(n - 1);
 }
 console.log(getn(10))
+//求斐波那契
+function fn(n) {
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    return fn(n - 1) + fn(n - 2);
+}
