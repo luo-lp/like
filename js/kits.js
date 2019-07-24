ef0a642281edda69f2d2436b20ffdda458d16b91
llp <875223115@qq.com>
2019-07-24 20:38:36 +0800

    需要一个可以把数组里面的每个函数都执行的方法

--- js/kits.js
+++ js/kits.js
@@ -34,3 +34,33 @@ function getRandom(n, m) {
     return Math.floor(Math.random() * n);
   }
 }
+// 状态模式的思想： 使用状态代替if-else
+function Validator() {
+  // 有一个数组，用来存储所有的验证的函数
+  this.validateFuncs = [];
+}
+// 2.2 给构造函数的原型添加一个方法，让其可以添加一个新的函数进去
+Validator.prototype.add = function (dom, arr) {
+  // 遍历数组，往this.validateFuncs 添加新的验证的方法
+  for (let i = 0; i < arr.length; i++) {
+    let fn = function () {
+      let rule = arr[i];
+      let params = rule.fnName.split(':'); // [minLength,8]
+      let fnName = params.shift(); // fnName里面可能会包含参数
+      params.unshift(dom.value); // [dom.vlaue,8]
+      params.push(rule.errMsg); // [dom.value,8,rule.errMsg];
+      return strategies[fnName].apply(dom, params);
+    }
+    this.validateFuncs.push(fn);
+  }
+
+}
+// 2.3 需要一个可以把数组里面的每个函数都执行的方法
+Validator.prototype.start = function () {
+  for (let i = 0; i < this.validateFuncs.length; i++) {
+    let msg = this.validateFuncs[i]();
+    if (msg) {
+      return msg;
+    }
+  }
+}
