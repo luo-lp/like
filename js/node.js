4d29e7456dc47e50683a33eb54915617e9dc6849
llp <875223115@qq.com>
2019-07-29 21:17:36 +0800

    读取json文件，返回数组的方法

--- js/node.js
+++ js/node.js
@@ -17,4 +17,20 @@
            // id 就是最大id -- 也是通过回调函数得到最大id
            callback(id);
        })
+       // 读取json文件，返回数组的方法
+       function getAllHero(callback) {
+           fs.readFile('./data/heros.json', 'utf-8', (err, data) => {
+               if (err) console.log(err);
+               let arr = JSON.parse(data);
+               // 要把数组返回 - 因为readFile是异步操作，会等待读取操作完成才执行
+               // 如果是异步操作，想要得到异步操作的结果 ： 唯一的办法，给一个回调函数
+               callback(arr);
+
+               // 回调函数相当于是下面的写法
+               // (function(array){
+               //   let html = template( __dirname + '/views/index.html',{ array });
+               //   res.end(html);
+               // })(arr);
+           })
+       }
    }
