047b924aef363456782f5ca3c5884f7f8bb6692a
llp <875223115@qq.com>
2019-07-29 21:15:58 +0800

    获取最大ID

--- js/node.js
+++ js/node.js
@@ -5,3 +5,16 @@
            if (err) console.log(err);
        })
    }
+   //获取最大ID方法
+   function getMaxId(callback) {
+       this.getAllHero((arr) => {
+           let id = arr[0].id;
+           for (let i = 1; i < arr.length; i++) {
+               if (arr[i].id > id) {
+                   id = arr[i].id;
+               }
+           }
+           // id 就是最大id -- 也是通过回调函数得到最大id
+           callback(id);
+       })
+   }
