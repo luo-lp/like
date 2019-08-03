17de482ad2df6fa22946d0d52bd22fffa5ec034c
luo-lp <875223115@qq.com>
2019-08-03 12:52:01 +0800

    添加bodyParser

--- 新建文件夹/练习/node&sql/app.js
+++ 新建文件夹/练习/node&sql/app.js
@@ -4,8 +4,19 @@ const express = require('express');
 const app = express();
 // 引入router模块
 const router = require('./router');
+// 引入bodyParser
+// const bodyParser = require('body-parser')
 // 绑定端口
 app.listen(3000, () => {
     console.log('服务器创建成功地址是:http://127.0.0.1:3000');
 })
+// 设置使用ejs模板
+app.set('view engine', 'ejs')
+// 设置body-parser
+// app.use(bodyParser.urlencoded({ extended: false }))
+// 使用router层
+// app.use(router)
+// 下载 npm i body-parser -S
+// const bodyParser = require('body-parser');
 
+// app.use(bodyParser.urlencoded({ extended: false }));
---
+++ 新建文件夹/练习/node&sql/router.js
@@ -0,0 +1,6 @@
+// 引入express
+const express = require('express');
+// 创建router
+const router = express.Router();
+// 让router暴露出来
+module.exporte  = router;
